03903392256767602e25802e859b3eba2cbecabc
Alvinluo1999917 <alvinluo.thant@gmail.com>
2020-08-23 14:29:25 +0630

    change header color

--- lib/_src/dropdown_header.dart
+++ lib/_src/dropdown_header.dart
@@ -46,7 +46,7 @@ class DropdownHeader extends DropdownWidget {
 class _DropdownHeaderState extends DropdownState<DropdownHeader> {
   Widget buildItem(
       BuildContext context, dynamic title, bool selected, int index) {
-    final Color primaryColor = Theme.of(context).primaryColor;
+    final Color primaryColor = Colors.red;
     final Color unselectedColor = Theme.of(context).unselectedWidgetColor;
     final GetItemLabel getItemLabel = widget.getItemLabel;
 
